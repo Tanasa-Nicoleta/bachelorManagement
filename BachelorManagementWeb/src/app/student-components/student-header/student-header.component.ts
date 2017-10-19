99db658356e9a5089f4b9519e54f400e8c446dcb
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2017-10-19 21:24:46 +0300

    A little progress on headers

---
+++ BachelorManagementWeb/src/app/student-components/student-header/student-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'student-header',
+  templateUrl: './student-header.component.html',
+  styleUrls: ['../../app.component.scss']
+})
+
+export class StudentHederComponent {
+}
---
+++ BachelorManagementWeb/src/app/teacher-components/teacher-header/teacher-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teacher-header',
+  templateUrl: './teacher-header.component.html',
+  styleUrls: ['../../app.component.scss']
+})
+
+export class TeacherHederComponent {
+}

ee3e0ef440e16b58382477df318ff2e482b28a91
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2017-10-19 20:48:58 +0300

    Work on bg image and renaming

--- BachelorManagementWeb/src/app/app.module.ts
+++ BachelorManagementWeb/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginLayoutComponent } from './login-components/login-layout/login-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginLayoutComponent
   ],
   imports: [
     BrowserModule

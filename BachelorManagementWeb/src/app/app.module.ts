5da29e2dedf41fd80a5c6801832f4b03b1712cac
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2017-10-21 17:00:00 +0300

    Added register component and defined default path

--- BachelorManagementWeb/src/app/app.module.ts
+++ BachelorManagementWeb/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginLayoutComponent } from './login-components/login-layout/login-layout.component';
+
+
+const appName = 'Bachelor Degree Management'
 
 @NgModule({
   declarations: [
@@ -10,9 +14,15 @@ import { LoginLayoutComponent } from './login-components/login-layout/login-layo
     LoginLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginLayoutComponent, pathMatch: 'full', data: { title: appName + 'Login' } },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
--- BachelorManagementWeb/src/app/login-and-register-components/login-layout/login-layout.component.ts
+++ BachelorManagementWeb/src/app/login-and-register-components/login-layout/login-layout.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'login-layout',
   templateUrl: './login-layout.component.html',
-  styleUrls: ['../../app.component.scss', './login-layout.component.scss']
+  styleUrls: ['../../app.component.scss', '../login-and-register-layout.component.scss']
 })
 
 export class LoginLayoutComponent {

15dcf5e803706d935363d6fdca7375bb77569410
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2017-11-04 14:03:14 +0200

    Check password matching, register page

--- BachelorManagementWeb/src/app/login-and-register-components/register-layout/register-layout.component.ts
+++ BachelorManagementWeb/src/app/login-and-register-components/register-layout/register-layout.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 
 export class RegisterLayoutComponent {
+
+  private faulted = false;
+  private invalidError = false;
+  private matchError = false;
+
+  passRegex: RegExp = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!*()\-_{}\\ |:;\,<>?`~\[\]\.\'])(?=\S+$).{6,32}$/;
+
+  private isValidPassword(pass: string) {
+    if (pass.length < 6 || !this.passRegex.test(pass) || pass.length > 32) {
+      return false;
+    }
+    return true;
+  }
+
+  private validatePass(password: HTMLInputElement) {
+    this.invalidError = !this.passRegex.test(password.value);
+    if (this.invalidError)
+      password.classList.add('invalidPass');
+    else
+      password.classList.remove('invalidPass');
+  }
+
+  DeleteError() {
+    this.invalidError = false;
+    this.matchError = false;
+  }
+
+  public confirmPass(password: HTMLInputElement, Password: string) {
+    if (password.value !== Password){     
+      password.classList.add('invalidPass');
+      this.matchError = true;
+      return false;
+    }
+    if (!this.isValidPassword(password.value)) {      
+      password.classList.add('invalidPass');
+      this.matchError = true;
+      return false;
+    }
+    else {
+      this.DeleteError();
+      return true;
+    }
+  }
 }

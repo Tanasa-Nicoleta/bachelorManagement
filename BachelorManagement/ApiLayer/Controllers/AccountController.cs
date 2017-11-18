af9929e2ad594d249a3aec38e9940ccb6d2ef653
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2017-11-18 15:12:00 +0200

    Added method to check at log in

--- BachelorManagement/ApiLayer/Controllers/AccountController.cs
+++ BachelorManagement/ApiLayer/Controllers/AccountController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using System.Net.Http;
+using ApiLayer.Models;
+using BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApiLayer.Controllers
 {
     public class AccountController : Controller
     {
-        
+        private readonly IAccountService AccountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            AccountService = accountService;
+        }
+
+        // POST: api/Account
+        public HttpResponseMessage Post([FromBody]AccountDTO accountDTO)
+        {
+            if (!ModelState.IsValid)
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            
+            if (!AccountService.CheckIfAccountExists(accountDTO.Username, accountDTO.Password))
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            
+            return new HttpResponseMessage(HttpStatusCode.OK);
+
+        }
     }
 }
---
+++ BachelorManagement/ApiLayer/Models/AccountDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiLayer.Models
+{
+    public class AccountDTO
+    {
+        [Required]
+        [EmailAddress]
+        [RegularExpression(@".+\b@info\.uaic\.com\b$")]
+        [MinLength(12)]
+        [MaxLength(61)]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!*()\-_{}\\ |:;\,<>?`~\[\]\.\'])(?=\S+$).{6,}$")]
+        [MaxLength(32)]
+        public string Password { get; set; }
+    }
+}

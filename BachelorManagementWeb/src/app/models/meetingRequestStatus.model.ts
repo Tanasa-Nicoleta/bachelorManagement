48b51b34a9674e1fdc2988fcff1a502ce86bb9a1
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2018-01-20 17:40:45 +0200

    Work on teacher wall

---
+++ BachelorManagementWeb/src/app/models/meetingRequestStatus.model.ts
@@ -0,0 +1,5 @@
+export enum MeetingRequestStatus{
+    Pending,
+    Accepted,
+    Declined
+}
--- BachelorManagementWeb/src/app/teacher-components/teacher-wall/teacher-wall.component.ts
+++ BachelorManagementWeb/src/app/teacher-components/teacher-wall/teacher-wall.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MeetingRequestStatus } from '../../models/meetingRequestStatus.model';
 
 @Component({
   selector: 'teacher-wall',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
 })
 
 export class TeacherWallComponent {
-  teacherName: string = "Teacher Name" // to be taken from api
+  teacherName: string = "Teacher Name"; // to be taken from api
+  availableDayAndHour: [string, string] = ["Monday", "12:00"];
+  requestAMeetingButton: string = "Request a meeting";
+  meetingRequest: boolean = false;
+  meetingResuestStatus: MeetingRequestStatus = MeetingRequestStatus.Pending;
+
+  requestMetting(){
+    if(this.meetingRequest){
+      this.meetingRequest = false;
+    }
+    else{
+      this.meetingRequest = true;
+    }
+    
+    if(this.requestAMeetingButton == "Request a meeting"){
+      this.requestAMeetingButton = "Cancel the meeting request";
+    }
+    else{
+      this.requestAMeetingButton = "Request a meeting";
+    }
+  }
 }

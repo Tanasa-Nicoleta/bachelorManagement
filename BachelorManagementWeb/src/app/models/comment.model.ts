c33dd9f7c475091b028932f711280303d6c865bf
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2018-01-24 12:37:45 +0200

    Start work on comments on teacher wall

---
+++ BachelorManagementWeb/src/app/models/comment.model.ts
@@ -0,0 +1,12 @@
+import { DateClass } from "./date.model";
+
+export class Comment{
+    constructor(name, content, timestamp){
+        this.name = name;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+    name: string;
+    content: string;
+    timestamp: DateClass
+}
--- BachelorManagementWeb/src/app/teacher-components/teacher-wall/teacher-wall.component.ts
+++ BachelorManagementWeb/src/app/teacher-components/teacher-wall/teacher-wall.component.ts
@@ -4,6 +4,7 @@ import { DayOfWeek } from '../../models/day-of-week.model';
 import { DateClass } from '../../models/date.model';
 import { TeacherObservation } from '../../models/teacher-observation.model';
 import { Month } from '../../models/month.model';
+import { Comment } from '../../models/comment.model';
 
 @Component({
   selector: 'teacher-wall',
@@ -13,20 +14,29 @@ import { Month } from '../../models/month.model';
 
 export class TeacherWallComponent {
   teacherName: string = "Teacher Name"; // to be taken from api
+  studentName: string = "Student Name";
   availableDay: string = DayOfWeek[0];
   upcomingDeadlineDay: DateClass =  new DateClass(1, Month[2], 2018, "14:00");
   availableHours: string = "12:00";
   requestAMeetingButton: string = "Request a meeting";
   meetingRequest: boolean = false;
   meetingRequestStatus: string = MeetingRequestStatus[0];
-  teacherObs: [TeacherObservation] = [new TeacherObservation("Hello everybody! Welcome to my page.", new DateClass(1, Month[1], 2018, "12:00")),
-                                        new TeacherObservation("Good morning! I want to see your papers soon.", new DateClass(20, Month[1], 2018, "14:40")),
-                                        new TeacherObservation("Hello! Any updates?", new DateClass(21, Month[1], 2018, "11:15")),
-                                        new TeacherObservation("Is anybody having a problem?", new DateClass(22, Month[1], 2018, "19:35")),
-                                        new TeacherObservation("I  have some news for you!" + 
+
+  firstDate= new DateClass(1, Month[1], 2018, "12:00");
+
+  firstTeacherObs = new TeacherObservation("Hello everybody! Welcome to my page.", this.firstDate);
+  secondTeacherObs = new TeacherObservation("Good morning! I want to see your papers soon.", new DateClass(20, Month[1], 2018, "14:40"));
+
+  teacherObs: [TeacherObservation] = [this.firstTeacherObs,
+                                      this.secondTeacherObs,
+                                      new TeacherObservation("Hello! Any updates?", new DateClass(21, Month[1], 2018, "11:15")),
+                                      new TeacherObservation("Is anybody having a problem?", new DateClass(22, Month[1], 2018, "19:35")),
+                                      new TeacherObservation("I  have some news for you!" + 
                                                                 "I'm not available to see you in person in the next two weeks because of some personal problems." +
                                                                 "I would be available on this platform so if you got ay questions plese shoot. :)", new DateClass(25,  Month[1], 2018, "21:30")),
-                                        new TeacherObservation("Good morning! I want to see your updated papers soon.", new DateClass(29, Month[1], 2018, "12:28"))];
+                                      new TeacherObservation("Good morning! I want to see your updated papers soon.", new DateClass(29, Month[1], 2018, "12:28"))];
+  comments: [[TeacherObservation, Comment]] = [[this.firstTeacherObs, new Comment(this.studentName, "Hello! Thank you!", this.firstDate)],
+                                              [this.secondTeacherObs, new Comment(this.studentName, "Hello! Thank you!", this.firstDate)]]                                  
 
   requestMetting(){
     if(this.meetingRequest){

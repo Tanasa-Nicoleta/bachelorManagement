dedc16774c20e6bbe891d7352e6a12e74272c46c
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2018-01-24 16:49:21 +0200

    Almost functional comments wih no style at teacher wall

--- BachelorManagementWeb/src/app/teacher-components/teacher-wall/teacher-wall.component.ts
+++ BachelorManagementWeb/src/app/teacher-components/teacher-wall/teacher-wall.component.ts
@@ -15,41 +15,48 @@ import { Comment } from '../../models/comment.model';
 export class TeacherWallComponent {
   teacherName: string = "Teacher Name"; // to be taken from api
   studentName: string = "Student Name";
+
   availableDay: string = DayOfWeek[0];
-  upcomingDeadlineDay: DateClass =  new DateClass(1, Month[2], 2018, "14:00");
   availableHours: string = "12:00";
+
+  upcomingDeadlineDay: DateClass = new DateClass(1, Month[2], 2018, "14:00");
+
   requestAMeetingButton: string = "Request a meeting";
   meetingRequest: boolean = false;
   meetingRequestStatus: string = MeetingRequestStatus[0];
 
-  firstDate= new DateClass(1, Month[1], 2018, "12:00");
+  teacherObs: [[TeacherObservation, [Comment | null]]] = [
+    [new TeacherObservation("Hello everybody! Welcome to my page.", new DateClass(1, Month[1], 2018, "12:00")),
+      [new Comment(this.studentName, "Hello! Thank you!", new DateClass(1, Month[1], 2018, "12:00")),
+      new Comment(this.studentName, "Hello! Thank you, mister " + this.teacherName + "!", new DateClass(20, Month[1], 2018, "14:40"))]],
+    [new TeacherObservation("Good morning! I want to see your papers soon.", new DateClass(20, Month[1], 2018, "14:40")),
+      [new Comment(this.studentName, "Hello! Thank you, mister " + this.teacherName + "!", new DateClass(20, Month[1], 2018, "14:40"))]],
+    // [new TeacherObservation("Hello! Any updates?", new DateClass(21, Month[1], 2018, "12:00")),
+    //   [null]],
+    // [new TeacherObservation("Is anybody having a problem?", new DateClass(22, Month[1], 2018, "19:35")),
+    //   [null]],
+    // [new TeacherObservation("I  have some news for you!" +
+    //   "I'm not available to see you in person in the next two weeks because of some personal problems." +
+    //   "I would be available on this platform so if you got ay questions plese shoot. :)",
+    //   new DateClass(25, Month[1], 2018, "21:30")),
+    //   [null]],
+    // [new TeacherObservation("Good morning! I want to see your updated papers on Monday.", new DateClass(29, Month[1], 2018, "12:28")),
+    //   [null]]
+  ];
 
-  firstTeacherObs = new TeacherObservation("Hello everybody! Welcome to my page.", this.firstDate);
-  secondTeacherObs = new TeacherObservation("Good morning! I want to see your papers soon.", new DateClass(20, Month[1], 2018, "14:40"));
 
-  teacherObs: [TeacherObservation] = [this.firstTeacherObs,
-                                      this.secondTeacherObs,
-                                      new TeacherObservation("Hello! Any updates?", new DateClass(21, Month[1], 2018, "11:15")),
-                                      new TeacherObservation("Is anybody having a problem?", new DateClass(22, Month[1], 2018, "19:35")),
-                                      new TeacherObservation("I  have some news for you!" + 
-                                                                "I'm not available to see you in person in the next two weeks because of some personal problems." +
-                                                                "I would be available on this platform so if you got ay questions plese shoot. :)", new DateClass(25,  Month[1], 2018, "21:30")),
-                                      new TeacherObservation("Good morning! I want to see your updated papers soon.", new DateClass(29, Month[1], 2018, "12:28"))];
-  comments: [[TeacherObservation, Comment]] = [[this.firstTeacherObs, new Comment(this.studentName, "Hello! Thank you!", this.firstDate)],
-                                              [this.secondTeacherObs, new Comment(this.studentName, "Hello! Thank you!", this.firstDate)]]                                  
-
-  requestMetting(){
-    if(this.meetingRequest){
+  requestMetting() {
+    if (this.meetingRequest) {
       this.meetingRequest = false;
     }
-    else{
+    else {
       this.meetingRequest = true;
     }
-    
-    if(this.requestAMeetingButton == "Request a meeting"){
+
+    if (this.requestAMeetingButton == "Request a meeting") {
       this.requestAMeetingButton = "Cancel the meeting request";
     }
-    else{
+    else {
       this.requestAMeetingButton = "Request a meeting";
     }
   }

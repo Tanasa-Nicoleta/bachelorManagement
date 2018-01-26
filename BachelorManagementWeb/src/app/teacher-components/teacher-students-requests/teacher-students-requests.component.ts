31296f9045bc3b19c7151e35a481befed1a67abc
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2018-01-26 11:50:46 +0200

    Work on teacher-student-requests page style

--- BachelorManagementWeb/src/app/teacher-components/teacher-students-requests/teacher-students-requests.component.ts
+++ BachelorManagementWeb/src/app/teacher-components/teacher-students-requests/teacher-students-requests.component.ts
@@ -11,11 +11,19 @@ import { Achievement } from '../../models/achievement.model';
 
 
 export class TeacherStudentsRequestsComponent {
-   studentList: [[Student, Achievement]] = [[new Student("Ana", "Maria", "A.M@info.uaic.ro",
-     new Bachelor("Title1", "Descripiton1")), new Achievement("My achievement")]]
+   studentList: [[Student, Achievement]] = [
+    [new Student("Ana", "Maria", "A.M@info.uaic.ro",
+      new Bachelor("Title1", "Descripiton1 and some random extra text")), 
+      new Achievement("My achievement 1")],
+    [new Student("Anita", "Mona", "A.M@info.uaic.ro",
+      new Bachelor("Title2", "Descripiton2")), 
+      new Achievement("My achievement and some other text")],
+    [new Student("Adriana", "Mihaela", "A.M@info.uaic.ro",
+      new Bachelor("Title3", "Descripiton3 and more words just to see how it looks on front")), 
+      new Achievement("My achievement and some other text too, just an example")]]
 
-    acceptStudentsRequest: string = "Accept request";
-    denyStudentsRequest: string = "Deny request";
+    acceptStudentsRequest: string = "Accept";
+    denyStudentsRequest: string = "Deny";
 
     acceptStudent(){
 

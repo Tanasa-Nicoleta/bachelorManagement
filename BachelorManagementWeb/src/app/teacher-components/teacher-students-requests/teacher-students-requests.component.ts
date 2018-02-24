8603d040b4fe688eac4da3f3383eeb3b08325f50
NicoletaTanasa <nicoleta.tanasa@info.uaic.ro>
2018-02-24 15:24:36 +0200

    Work on teacehr-students-request look and functionality

--- BachelorManagementWeb/src/app/teacher-components/teacher-students-requests/teacher-students-requests.component.ts
+++ BachelorManagementWeb/src/app/teacher-components/teacher-students-requests/teacher-students-requests.component.ts
@@ -14,20 +14,24 @@ export class TeacherStudentsRequestsComponent {
     numberOfSpots: number = 12;
     numberOfAvailableSpots: number = 10;
     studentAccepted: boolean = false;
+    studentDenied: boolean = false;
 
-   studentList: [[Student, Achievement, boolean]] = [
+   studentList: [[Student, Achievement, boolean, boolean]] = [
     [new Student("Ana", "Maria", "An.Ma@info.uaic.ro",
       new Bachelor("Title1", "Descripiton1 and some random extra text")), 
       new Achievement("My achievement 1"),
-    this.studentAccepted],
+    this.studentAccepted,
+    this.studentDenied],
     [new Student("Anita", "Mona", "An.Mo@info.uaic.ro",
       new Bachelor("Title2", "Descripiton2")), 
       new Achievement("My achievement and some other text"),
-      this.studentAccepted],
+      this.studentAccepted,
+      this.studentDenied],
     [new Student("Adriana", "Mihaela", "Ad.Mi@info.uaic.ro",
       new Bachelor("Title3", "Descripiton3 and more words just to see how it looks on front")), 
       new Achievement("My achievement and some other text too, just an example"),
-      this.studentAccepted]]
+      this.studentAccepted,
+      this.studentDenied]];
 
     acceptStudentsRequest: string = "Accept";
     denyStudentsRequest: string = "Deny";
@@ -42,7 +46,11 @@ export class TeacherStudentsRequestsComponent {
       });
     }
 
-    denyStudent(){
-
-    }
+    denyStudent(student: Student){
+      this.studentList.forEach(stud => {
+        if(stud[0].Email == student.Email){
+          stud[3] = stud[3] == true ? false : true;
+        }
+    });
+  }
 }
